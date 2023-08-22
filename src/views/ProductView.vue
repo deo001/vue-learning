52f51f3e29bfe51f34da0e10a8a20ed2f6e86924
brellah codes <brellahcodes@gmail.com>
2023-08-22 18:25:48 +0300

    added bootstrap extension

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,9 +1,10 @@
 <template>
     <div>
         .<div class="card">
+            <img src="../assets/image/1.jpg" class="fluid-img " width="300"  alt="">
             <div class="card-body">
-                <h4 class="card-title">Germany Coat</h4>
-                <p class="card-text">Get the affordable coat for this winter</p>
+                <h4 class="card-title w-75 align-item-center btn btn-primary">Germany Coat</h4>
+                <p class="card-text text-center text-muted">Get the affordable coat for this winter</p>
             </div>
         </div>
     </div>

fa678e372858f15381e7c9725cd91096db6ad805
brellah codes <brellahcodes@gmail.com>
2023-08-22 16:35:54 +0300

    my first vue file just installation

---
+++ src/views/ProductView.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        .<div class="card">
+            <div class="card-body">
+                <h4 class="card-title">Germany Coat</h4>
+                <p class="card-text">Get the affordable coat for this winter</p>
+            </div>
+        </div>
+    </div>
+</template>
